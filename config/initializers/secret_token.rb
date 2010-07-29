65374cf972a70a6d0245c611d311bedaa812790f
Paul Morganthall <slothbear@constella.org>
2010-07-29 14:45:39 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kimballtron::Application.config.secret_token = 'ffb14a023a4f8d2aa54fe22f8739b20255805c9fb8edd6e90709bf32006fdd827ae655b9b0fcf123f49053f1eced85710f0f69966075cfb0b6215a5839d95d02'
