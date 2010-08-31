f885ca93fe0a1c811e5cafc339bb3ca18c70b87d
Paul Morganthall <slothbear@constella.org>
2010-08-31 17:23:07 -0400

    implement farmers/show incl. Farm.create

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,3 +1,20 @@
 class Farm < ActiveRecord::Base
   belongs_to :farmer
+  after_initialize :set_defaults
+
+  def set_defaults
+   self.plots ||= 350
+   self.harvests_per_day ||= 2
+  end
+
+  private
+  def self.new_farm_name
+    keywords = YAML::load_file(Rails.root.join('config', 'farm_names.yml'))
+    farm_types = keywords["farm_types"]
+    level_names = keywords["level_names"]
+
+    prefix = level_names[rand(level_names.size)]
+    suffix = farm_types[rand(farm_types.size)]
+    prefix + " " + suffix
+  end
 end
