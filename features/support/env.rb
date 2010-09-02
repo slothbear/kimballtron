ee7341faef45525f0bc0e8f844d3097804fd6827
Paul Morganthall <slothbear@constella.org>
2010-09-01 21:56:52 -0400

    Use seeds.rb instead of fixtures.

--- features/support/env.rb
+++ features/support/env.rb
@@ -64,7 +64,4 @@ require 'cucumber/rails/world'
 Cucumber::Rails::World.use_transactional_fixtures
 
 #Seed the DB
-Fixtures.reset_cache  
-fixtures_folder = File.join(Rails.root, 'spec', 'fixtures')
-fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
-Fixtures.create_fixtures(fixtures_folder, fixtures)
+require "#{Rails.root}/db/seeds.rb"
