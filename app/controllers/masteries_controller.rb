86ae91fd670ddbd3e75ee9bfe8ec1818cb4b3cb4
Paul Morganthall <slothbear@constella.org>
2010-09-09 11:50:16 -0400

    Support cancel button when editing masteries.

--- app/controllers/masteries_controller.rb
+++ app/controllers/masteries_controller.rb
@@ -4,13 +4,20 @@ class MasteriesController < ApplicationController
   end
 
   def update
-    @masteries = Mastery.update(params[:masteries].keys, params[:masteries].values).reject {|m| m.errors.empty?}
-    
-    if @masteries.empty?   
-      flash[:notice] = "masteries updated"
+    if params['cancel']
+      flash[:notice] = "edit cancelled; no masteries were updated."
       redirect_to farmer_path(current_farmer)
-    else  
-      render :action => 'edit'  
-    end
-  end
-end
+    else
+      @masteries = Mastery.update(params[:masteries].keys,
+        params[:masteries].values).reject {|m| m.errors.empty?}
+
+      if @masteries.empty?
+        flash[:notice] = "masteries updated"
+        redirect_to farmer_path(current_farmer)
+      else
+        render :action => 'edit'
+      end
+
+    end # if cancel
+  end # update
+end # class
