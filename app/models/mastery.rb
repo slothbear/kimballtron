33a5e89570394f060e21521054e32f595cd959ab
Paul Morganthall <slothbear@constella.org>
2010-09-09 11:54:27 -0400

    sort by remaining L2 harvests; stripe rows

--- app/models/mastery.rb
+++ app/models/mastery.rb
@@ -15,6 +15,14 @@ class Mastery < ActiveRecord::Base
       :greater_than_or_equal_to => 0,
       :only_integer => true
 
+  def remaining_harvests # for level 2 harvests for now
+    (crop.mastery_2 - additional_harvests)
+  end
+
+  def h_time
+    crop.harvest_time
+  end
+
   def set_defaults
     return unless new_record?
     self.stars ||= 0
