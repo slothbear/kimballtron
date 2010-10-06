3fa6a33aa18f68aa6b4c73b75da20c2d598a507e
Paul Morganthall <slothbear@constella.org>
2010-10-05 22:37:27 -0400

    Read crop seed from yaml.  market_orders revised.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,6 @@
-# Name,marketOrder,level,hours,cost,sell_for,xp,type,M1,M2,M3
-File.readlines("#{Rails.root}/db/crops.csv").each do |line|
-    name, market_order, level, buy,
-    harvest_time, sell, xp, family,
-    mastery_1, mastery_2, mastery_3 = line.split(",")
+crop_data = YAML::load_file("#{Rails.root}/db/crops.yaml")
 
+crop_data.each do |name, attrs|
   crop = Crop.find_or_create_by_name(name)
-  crop.update_attributes(
-     :level => level,
-     :harvest_time => harvest_time,
-     :buy => buy,
-     :sell => sell,
-     :xp => xp,
-     :family => family,
-     :mastery_1 => mastery_1,
-     :mastery_2 => mastery_2,
-     :mastery_3 => mastery_3
-   )
-
-# "Farmvilel Profit Sheet" doesn't include (my) market_order, so existing
-# records won't have market_order.  Just the new ones added this time.
-# TODO: crufty; rethink crop import strategy.
-  if market_order != ""
-    crop.update_attributes(:market_order => market_order)
-  end
+  crop.update_attributes(attrs)
 end
